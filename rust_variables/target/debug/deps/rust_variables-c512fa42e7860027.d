7c91f28007b677bf61e3494036425eacaba9a9b3
kupuma-ru21 <kupumaru21@gmail.com>
2022-01-22 23:55:10 +0900

    feat: variables

---
+++ rust_variables/target/debug/deps/rust_variables-c512fa42e7860027.d
@@ -0,0 +1,5 @@
+/Users/kimurakouichi/Project/rust_variables/target/debug/deps/rust_variables-c512fa42e7860027: src/main.rs
+
+/Users/kimurakouichi/Project/rust_variables/target/debug/deps/rust_variables-c512fa42e7860027.d: src/main.rs
+
+src/main.rs:
---
+++ rust_variables/target/debug/rust_variables.d
@@ -0,0 +1,1 @@
+/Users/kimurakouichi/Project/rust_variables/target/debug/rust_variables: /Users/kimurakouichi/Project/rust_variables/src/main.rs
